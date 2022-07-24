12f1576c65e1ce1b746ad2abdf67575df5c78fdd
elis-11 <eliza.arzanukaeva@gmail.com>
2022-07-24 11:19:39 +0200

    Client: Created & Styled- Register & Login

--- posts/src/components/Navbar.jsx
+++ posts/src/components/Navbar.jsx
@@ -11,50 +11,47 @@ export const Navbar = () => {
   return (
     <div className="flex py-4 justify-between items-center">
       <span className="flex justify-center items-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm">
+        {" "}
         E
       </span>
 
       {isAuth && (
-      <ul className="flex gap-8">
-        <li>
-          <NavLink
-            to={"/"}
-            href="/"
-            className="text-xs text-gray-400 hover:text-white"
-            style={({ isActive }) => (isActive ? activeStyles : undefined)}
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to={"/posts"}
-            href="/"
-            className="text-xs text-gray-400 hover:text-white"
-            style={({ isActive }) => (isActive ? activeStyles : undefined)}
-          >
-            Posts
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to={"/new"}
-            href="/"
-            className="text-xs text-gray-400 hover:text-white"
-            style={({ isActive }) => (isActive ? activeStyles : undefined)}
-          >
-            Add Posts
-          </NavLink>
-        </li>
-      </ul>
-)} 
+        <ul className="flex gap-8">
+          <li>
+            <NavLink
+              to={"/"}
+              href="/"
+              className="text-xs text-gray-400 hover:text-white"
+              style={({ isActive }) => (isActive ? activeStyles : undefined)}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/posts"}
+              href="/"
+              className="text-xs text-gray-400 hover:text-white"
+              style={({ isActive }) => (isActive ? activeStyles : undefined)}
+            >
+              Posts
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/new"}
+              href="/"
+              className="text-xs text-gray-400 hover:text-white"
+              style={({ isActive }) => (isActive ? activeStyles : undefined)}
+            >
+              Add Posts
+            </NavLink>
+          </li>
+        </ul>
+      )}
 
       <div className="flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm px-4 py-2">
-        {isAuth ? (
-            <button>Logout</button>
-        ) : (
-            <Link to={'/login'}> Login </Link>
-        )} 
+        {isAuth ? <button>Logout</button> : <Link to={"/login"}> Login </Link>}
       </div>
     </div>
   );
